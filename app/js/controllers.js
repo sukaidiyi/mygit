1024eacf067d31f268accad708cc3bf564fd4b30
sukaidiyi <sukaidiyi@yeah.net>
2015-10-29 10:20:53 +0800

    可以将题目展示出来了

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,8 +4,18 @@
 
 var myApp = angular.module("myApp",[])
 
-myApp.controller('myAppCtrl',function($scope)
-	$scope.Topics = 
-{
-	"title" : "1+1=？"
+/* myApp.controller("myAppCtrl",function($scope){
+	$scope.topic = {"title" : "1+1=?",
+	"options" = [ {"content" : 2, "correct": "right"},
+	{"content" : 3, "correct": "false"}
+	]};
+})*/
+
+var options = [
+	{"option" : "2", "correct" : "true"},
+	{"option" : "3", "correct" : "false"}
+];
+myApp.controller("myAppCtrl",function($scope){
+	$scope.topic = {"title":"1+1=?",
+					"options" : options};
 })
